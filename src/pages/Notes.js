f71fc4c05f076a2f8fbb263c5965d2137d000ff1
sugianto43 <antho9922@gmail.com>
2021-06-24 12:02:45 +0700

    final commit

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Container } from "@material-ui/core";
+import NoteCard from "../components/NodeCard";
+import Masonry from "react-masonry-css";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -8,11 +11,35 @@ export default function Notes() {
       .then((res) => res.json())
       .then((data) => setNotes(data));
   }, []);
+
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:8000/notes/${id}`, {
+      method: "DELETE",
+    });
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  const breakpoints ={
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
-    <div>
-      {notes.map((note) => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {notes.map((note) => (
+          <div key={note.id}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </div>
+        ))}
+      </Masonry>
+    </Container>
   );
 }
